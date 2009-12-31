a00192b4a9b2ac6323eb01241ba80937c1d1d187
Leon Gersing <fallenrogue@gmail.com>
2009-12-31 14:50:34 -0500

    facebooker/jquery integration

--- config/environment.rb
+++ config/environment.rb
@@ -21,6 +21,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'facebooker'
   config.gem 'twitter'
+  config.gem 'aaronchi-jrails', :lib=>'jrails'
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
