0cddc707e40f24a7e9dc00217c45cc225010d1b2
Leon Gersing <fallenrogue@gmail.com>
2010-01-11 14:14:16 -0500

    adding another tiny bug fix

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -19,6 +19,6 @@ class UserSessionsController < ApplicationController
   def destroy
     current_user_session.destroy
     flash[:notice] = "Logout successful!"
-    redirect_back_or_default new_user_session_url
+    redirect_to new_user_session_url
   end  
 end
