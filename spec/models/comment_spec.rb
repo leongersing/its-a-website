9c616b0274e8c1a2eae6c5b0166b9f6b760d4fed
Adam Winter <adam@joemetric.com>
2010-01-13 12:34:22 -0500

    added tests for being able to store descriptive texts

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -7,6 +7,7 @@ describe Comment do
 
     @public_comment = Comment.new
     @public_comment.private = false
+    @public_comment.body="This is some text"
   end
 
   it "should be a private comment" do
@@ -16,10 +17,13 @@ describe Comment do
   it "should be a public comment" do
     @public_comment.should_not be_private    
   end
+
+  it "should start out with an empty (nil) comment" do
+    @private_comment.body.should be_nil        
+  end
   
-  
-  # it "should be able to store descriptive text" do
-    # !@public_comment.private?
-  # end
+  it "should be able to store descriptive text" do
+    @public_comment.body.should_not be_empty        
+  end
   
 end
