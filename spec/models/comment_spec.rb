f59faf40cfed0e883ee539d5e3978f2c57dba752
Adam Winter <adam@joemetric.com>
2010-01-13 13:42:42 -0500

    refactoring my comment tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,28 +2,29 @@ require 'spec_helper'
 
 describe Comment do
   before(:each) do
-    @private_comment = Comment.new
-    @private_comment.private = true
-
-    @public_comment = Comment.new
-    @public_comment.private = false
-    @public_comment.body="This is some text"
   end
 
   it "should be a private comment" do
+    @private_comment = Comment.new
+    @private_comment.private = true
     @private_comment.should be_private
   end
 
   it "should be a public comment" do
+    @public_comment = Comment.new
+    @public_comment.private = false
     @public_comment.should_not be_private    
   end
 
   it "should start out with an empty (nil) comment" do
-    @private_comment.body.should be_nil        
+    @comment = Comment.new
+    @comment.body.should be_nil        
   end
   
   it "should be able to store descriptive text" do
-    @public_comment.body.should_not be_empty        
+    @comment = Comment.new
+    @comment.body="This is some text"
+    @comment.body.should_not be_empty        
   end
   
 end
