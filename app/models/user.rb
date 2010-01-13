bbafce3429f31f6ad3aea8f658d9e21ae2ba14c0
Leon Gersing <fallenrogue@gmail.com>
2010-01-13 16:38:26 -0500

    merging jerry's branch

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,31 @@ class User < ActiveRecord::Base
   has_many :ratings
   belongs_to :presenter
   has_many :comments
+  
+  # SEE http://railscasts.com/episodes/189-embedded-association for info
+  # about how roles are implemented.
+  
+  ROLES = %w[attendee speaker organizer]
+  
+  named_scope :with_role, lambda { |role| {:conditions => "roles_mask & #{2**ROLES.index(role.to_s)} > 0"} }
+  
+  # returns whether a user has a particular role
+  def has_role?(role)
+    roles.include? role
+  end
+  
+  # assigns an array of string roles
+  def roles=(roles)
+    self.roles_mask = (roles & ROLES).map { |r| 2**ROLES.index(r) }.sum
+  end
+  
+  # returns an array of strings that represent the user's roles
+  def roles
+    ROLES.reject { |r| ((roles_mask || 0) & 2**ROLES.index(r)).zero? }
+  end
+    
+  # symbolize roles
+  def role_symbols
+    roles.map(&:to_sym)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,5 +18,68 @@ describe User do
     @user1.comments << @comment2
     @user1.comments.size.should == 2
   end
+end  
+
+describe "User role instance methods" do
+  before(:each) do
+    @user = Factory.create(:user)
+  end
+  
+  describe "with no role" do
+    it 'should have no roles' do
+      @user.roles.should == []
+    end
+  end
+  
+  describe "with a single role" do
+    before(:each) do
+      @user.roles = ['attendee']
+    end
+
+    it 'can assign one role' do
+      @user.roles.should == ['attendee']
+    end
+
+    it 'determines if the user has a role' do
+      @user.has_role?('attendee').should be_true
+    end
+  end
+  
+  describe 'with multiple roles' do
+    before(:each) do
+      @user.roles = ['attendee', 'speaker']
+    end
     
+    it 'can assign multiple roles' do
+      @user.should have(2).roles
+      @user.roles.should include('attendee')
+      @user.roles.should include('speaker')
+    end
+    
+    it 'determines if the user has the role' do
+      @user.should have_role('attendee')
+      @user.should have_role('speaker')
+    end
+  end
+end
+
+describe "User role class methods" do
+  it 'returns all users for a particular role' do
+    attendee = Factory.create(:user, :roles => ['attendee'])
+    speaker = Factory.create(:user, :roles => ['speaker'])
+    
+    User.with_role('attendee').should include(attendee)
+  end
+  
+  it 'returns users when they have multiple roles' do
+    attendee = Factory.create(:user, :roles => ['attendee'])
+    speaker_attendee = Factory.create(:user, :roles => ['speaker', 'attendee'])
+    Factory.create(:user, :roles => ['organizer'])
+    
+    attendees = User.with_role('attendee')
+    
+    attendees.should have(2).attendees
+    attendees.should include(attendee)
+    attendees.should include(speaker_attendee)
+  end
 end
