b8ba7e8936b83a0999e3c025288d93012c3741d6
tonyschneider <tsnydermtg@gmail.com>
2010-01-13 14:18:05 -0500

    created ability to rate presentations

--- app/models/presentation.rb
+++ app/models/presentation.rb
@@ -1,2 +1,18 @@
 class Presentation < ActiveRecord::Base
+  has_many :ratings
+  
+  def rate_thumbs_up(user)
+    if self.ratings.find_all_by_user_id(user).size == 0
+      self.ratings.create(:user => user, :thumbs_up => true)
+      self.positive_ratings += 1
+    end
+  end
+  
+  def rate_thumbs_down(user)
+    if self.ratings.find_all_by_user_id(user).size == 0
+      self.ratings.create(:user => user, :thumbs_up => false)
+      self.negative_ratings += 1
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   acts_as_authentic  
+  has_many :ratings
 end
 
