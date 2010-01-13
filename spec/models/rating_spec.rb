7c8754f0fcdc22a10711041adbb6142435d34748
Joe Fiorini <joe@faithfulgeek.org>
2010-01-13 16:23:54 -0500

    Fix spec_helper paths in each spec

--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 require 'rating'
 
 describe Rating do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 require 'user'
 
 describe User do
