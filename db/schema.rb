3602c6991c5ac8196583f9a93b131d501ce49fd4
swalke16 <swalke16@gmail.com>
2010-01-13 12:15:53 -0500

    added basic fields to conference

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100107174552) do
+ActiveRecord::Schema.define(:version => 20100113165858) do
 
   create_table "categories", :force => true do |t|
     t.string   "title"
@@ -28,6 +28,9 @@ ActiveRecord::Schema.define(:version => 20100107174552) do
   create_table "conferences", :force => true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
+    t.datetime "start_date"
+    t.datetime "end_date"
   end
 
   create_table "presentations", :force => true do |t|
--- spec/models/speaker_spec.rb
+++
@@ -1,4 +0,0 @@
-require 'spec_helper'
-
-describe Speaker
-end
