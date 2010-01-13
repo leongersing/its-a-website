8b4d626817d685e3eeb30e8d6ba87510162e8717
Joe Fiorini <joe@faithfulgeek.org>
2010-01-13 16:19:03 -0500

    Add testing gem requirements

--- Gemfile
+++ Gemfile
@@ -5,3 +5,10 @@ gem "twitter"
 gem "jrails"
 gem "authlogic"
 gem "formtastic"
+
+only :test do
+  gem "rspec", :require_as => "spec"
+  gem "rspec-rails", :require_as => nil
+  gem "mocha"
+  gem "factory_girl"
+end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,3 +1,4 @@
+Bundler.require_env :test
 # Settings specified here will take precedence over those in config/environment.rb
 
 # The test environment is used exclusively to run your application's
@@ -26,6 +27,3 @@ config.action_mailer.delivery_method = :test
 # This is necessary if your schema can't be completely dumped by the schema dumper,
 # like if you have constraints or database-specific column types
 # config.active_record.schema_format = :sql
-config.gem 'mocha'
-config.gem "factory_girl", :source => "http://gemcutter.org"
-
