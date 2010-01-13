b5a77c3a7edee01ad6947db4f4cb788f32aa45c1
Adam Winter <adam@joemetric.com>
2010-01-13 12:28:35 -0500

    fixed and optimized test

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,18 +3,23 @@ require 'spec_helper'
 describe Comment do
   before(:each) do
     @private_comment = Comment.new
-    @private_comment.private == true
+    @private_comment.private = true
 
     @public_comment = Comment.new
-    @public_comment.private == false
+    @public_comment.private = false
   end
 
   it "should be a private comment" do
-    @private_comment.private?
+    @private_comment.should be_private
   end
 
   it "should be a public comment" do
-    !@public_comment.private?
+    @public_comment.should_not be_private    
   end
-
+  
+  
+  # it "should be able to store descriptive text" do
+    # !@public_comment.private?
+  # end
+  
 end
