27d1313f5f49de40ed4f5f236bd0775f2861bb43
Craig Bruenderman <craig@resonantvibes.com>
2010-01-13 14:30:51 -0500

    comment tests

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,19 @@
 class CommentsController < ApplicationController
   
   def create
+    if current_user.blank?
+      flash[:error] = 'User not logged in'
+      redirect_to :root
+    else
+      comment = Comment.create(:subject => params[:comment][:subject], :user => current_user, :body => params[:comment][:body] )
+      if comment.is_valid?
+        flash[:success] = 'Comment created!'
+      else
+        flash[:error] = 'Comment not created'
+      end
+      redirect_to :back
+    end
+      
   end
   
 end
