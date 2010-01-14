56c6471391b6aafbdac88b18d36d40e9b3e71893
Leon Gersing <fallenrogue@gmail.com>
2010-01-14 01:57:47 -0500

    seeding

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,5 +1,5 @@
 class UserSessionsController < ApplicationController
-  before_filter :require_no_user, :only => [:new, :create]
+  before_filter :require_no_user, :only => [:create]
   before_filter :require_user, :only => :destroy
   
   def new
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+
+class CodeMashImporter
+  include HTTParty
+end
+
+def clean_date(date)
+  begin
+    date.to_date
+  rescue
+    date = Time.now
+  end
+  date
+end
+
+CodeMashImporter.get("http://codemash.org/rest/speakers.json").each do |s|
+  Presenter.create(:name            => s["Name"],
+                   :reference_data  => s["SpeakerURI"],
+                   :bio             => s["Biography"],
+                   :contact_info    => s["ContactInfo"],
+                   :blog_url        => s["BlogURL"],
+                   :twitter_name    => s["TwitterHandle"]
+                   )
+end
+
+CodeMashImporter.get("http://codemash.org/rest/sessions.json").each do |s|
+  p = Presentation.new(:title            =>  s["Title"],
+                          :description      =>  s["Abstract"],
+                          :start_time       =>  clean_date(s["Start"]),
+                          :location         =>  s["Room"].blank? ? "NA" : s["Room"],
+                          :level            =>  s["Difficulty"],
+                          :technology       =>  s["Technology"],
+                          :category         =>  Category.find_or_create_by_title(s["Track"]),
+                          :reference_data   =>  s["URI"])
+                          
+  speaker = Presenter.find_by_name(s["SpeakerName"])
+  p.presenters << speaker unless speaker.blank?
+  p.save!
+end
