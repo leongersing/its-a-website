fb6eb3ab73fe9e39a8a3b7c0613d5f77f0566538
Cory Flanigan <seeflanigan@gmail.com>
2010-01-14 21:35:23 -0500

    User table should not have presenter_id

--- db/migrate/20100113191816_presenters_user.rb
+++
@@ -1,9 +0,0 @@
-class PresentersUser < ActiveRecord::Migration
-  def self.up
-    add_column :users, :presenter_id, :integer
-  end
-
-  def self.down
-    drop_column :users, :presenter_id
-  end
-end
