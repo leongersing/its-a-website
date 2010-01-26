1e738a5c1b1f9149105ec93abac8aba0712f64c6
Ben Wagaman <jamin4jc@rubicon.(none)>
2010-01-25 20:22:32 -0500

    minor mods

--- spec/models/conference_spec.rb
+++ spec/models/conference_spec.rb
@@ -2,6 +2,6 @@ require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 
 describe Conference do
   it { should be_a Conference }
-  it { subject.should respond_to(:user) }
+  it { subject.should belong_to :user }
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 
 describe User do
-  it { should respond_to :conferences }
+  it { should have_many :conferences }
 end
 
 describe User do
