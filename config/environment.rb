0eb2e9cff08460bb430c2c123e9cbb143c757f02
Leon Gersing <leon@edgecase.com>
2011-01-10 14:25:01 -0500

    init

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Precomp2011::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Precomp2011::Application.config.secret_token = '1ee690acedcf5821901cb4be71be74234e47609e83226c9c44e15f12d4f5e41a5f741bf44fb79fc3be4d2fce5409abf431a5ab3cb8071ce8fef5cd74466992c6'
