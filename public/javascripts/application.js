55fa0d117b3016875e465566601f2b89fe57d9b2
Shawn Wallace <shawnewallace@gmail.com>
2011-01-12 16:15:35 -0500

    refactored javascript to be prettier and easily read

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,38 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+  $(document).ready(function() {
+    $('select#PresentationSort').change(function(){ 
+        sortUl($(this).val());
+      });
+     
+    $('input#FilterPresentationTextbox').bind('change keyup', function() {
+      var searchValue = $(this).val().trim();
+      
+      if (searchValue == '')
+        {
+          $('li').show();
+          return;          
+        }
+      
+      $('li').not(":contains('" + searchValue + "')'").hide('slow');
+      $('li:contains("' + searchValue + '")').show('slow');  
+    });
+    
+    sortUl('data-title');
+  });
+   
+   function sortUl(sortColumn){
+     var mylist = $("ul");
+     
+     var listItems = mylist.children('li').get();
+     
+     listItems.sort(function(a, b){
+        
+        var compA = $(a).attr(sortColumn).toUpperCase();
+        var compB = $(b).attr(sortColumn).toUpperCase();
+        
+        return (compA < compB) ? -1 : (compA > compB) ? 1 : 0;
+      });
+     
+     $.each(listItems, function(idx, itm){ mylist.append(itm); });
+   }
