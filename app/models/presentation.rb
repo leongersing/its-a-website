eef6b9e7012927cf4dd462b11bd09d3b491a44d5
Leon Gersing <leon@edgecase.com>
2011-01-12 10:53:28 -0500

    Added Create and Read for presentation model

--- app/models/presentation.rb
+++ app/models/presentation.rb
@@ -1,2 +1,5 @@
 class Presentation < ActiveRecord::Base
+  validates_presence_of :name
+  validates_presence_of :starts_at
+  validates_presence_of :speaker_name
 end
--- db/migrate/20110112150507_create_presentations.rb
+++ db/migrate/20110112150507_create_presentations.rb
@@ -1,6 +1,12 @@
 class CreatePresentations < ActiveRecord::Migration
   def self.up
     create_table :presentations do |t|
+      t.string :name
+      t.string :description
+      t.string :room
+      t.integer :duration
+      t.string :speaker_name
+      t.datetime :starts_at
 
       t.timestamps
     end
