b322b8913e0cfe2959ee0542a2f1513eaf61a52b
Mike Busch <busch.michael@gmail.com>
2011-01-12 22:59:48 -0500

    added a backdoor for local development to authenticate using a user's name

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Precomp2011::Application.routes.draw do
   resources :presentations do
      resources :presentation_questions
   end
-  
+
   resources :users
 
   match '/auth/:provider/callback', :to => 'sessions#create'
   match '/sessions/destroy', :to => 'sessions#destroy'
+  match '/sessions/create', :to => 'sessions#create_by_name' if Rails.env.development?
   root :to => "presentations#index"
 end
