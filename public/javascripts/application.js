eb37ec2ce32a9fc6b3f2db91645a5d1bdd877700
Mike Busch <busch.michael@gmail.com>
2011-01-12 22:22:03 -0500

    case insensitive filtering

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,38 +1,46 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-  $(document).ready(function() {
-    $('select#PresentationSort').change(function(){ 
-        sortUl($(this).val());
-      });
-     
-    $('input#FilterPresentationTextbox').bind('change keyup', function() {
-      var searchValue = $(this).val().trim();
-      
-      if (searchValue == '')
-        {
-          $('li').show();
-          return;          
-        }
-      
-      $('li').not(":contains('" + searchValue + "')'").hide('slow');
-      $('li:contains("' + searchValue + '")').show('slow');  
+jQuery(function($) {
+  $('select#PresentationSort').change(function(){
+      sortUl($(this).val());
     });
-    
-    sortUl('data-title');
-  });
-   
-   function sortUl(sortColumn){
-     var mylist = $("ul");
-     
-     var listItems = mylist.children('li').get();
-     
-     listItems.sort(function(a, b){
-        
-        var compA = $(a).attr(sortColumn).toUpperCase();
-        var compB = $(b).attr(sortColumn).toUpperCase();
-        
-        return (compA < compB) ? -1 : (compA > compB) ? 1 : 0;
+
+  $('input#FilterPresentationTextbox').bind('change keyup', function() {
+    var show = $(),
+        hide = $(),
+        searchValue = $(this).val().trim(),
+        regex = new RegExp(searchValue, "gi");
+
+    if(searchValue == '') {
+      show = $('li');
+    } else {
+      $('li').each(function() {
+        var isMatch = $(this).html().toLowerCase().match(regex);
+        if(isMatch) {
+          show = show.add(this);
+        } else {
+          hide = hide.add(this);
+        }
       });
-     
-     $.each(listItems, function(idx, itm){ mylist.append(itm); });
-   }
+    }
+    show.show('slow');
+    hide.hide('slow');
+  });
+
+  sortUl('data-title');
+
+});
+
+function sortUl(sortColumn){
+  var mylist = $("ul");
+
+  var listItems = mylist.children('li');
+
+  listItems.sort(function(a, b){
+    var compA = $(a).attr(sortColumn).toUpperCase();
+    var compB = $(b).attr(sortColumn).toUpperCase();
+    return (compA < compB) ? -1 : (compA > compB) ? 1 : 0;
+  });
+
+  $.each(listItems, function(idx, itm){ mylist.append(itm); });
+}
